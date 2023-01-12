0b059ce7816b75e62d61f0cec0588ed3fb55d798
Paul Jha <paul.hutalla@gmail.com>
2023-01-11 17:03:22 -0800

    added front & backend folder

---
+++ backend/app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+require("express-async-errors");
+const morgan = require("morgan");
+const cors = require("cors");
+const csurf = require("csurf");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const {environment} = require("./config");
+const isProduction = environment === "production";
+const app = express();
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(express.json());
+
+// Security Middleware
+if (!isProduction) {
+  // enable cors only in development
+  app.use(cors());
+}
+
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin"
+  })
+);
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
+
+// backend/app.js
+const routes = require('./routes');
+
+// ...
+
+app.use(routes); // Connect all the routes
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+module.exports = app;
---
+++ backend/routes/api/index.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+
+// backend/routes/api/index.js
+// ...
+
+router.post('/test', function(req, res) {
+  res.json({ requestBody: req.body });
+});
+
+// ...
+
+module.exports = router;
