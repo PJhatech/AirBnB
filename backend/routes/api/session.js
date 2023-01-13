e88279f43c739a76cc783ad267e127cfe0bd19b1
Paul Jha <paul.hutalla@gmail.com>
2023-01-12 17:51:48 -0800

    phase 5

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -37,4 +37,19 @@ router.delete(
   }
 );
 
+// Restore session user
+router.get(
+  '/',
+  restoreUser,
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      return res.json({
+        user: user.toSafeObject()
+      });
+    } else return res.json({ user: null });
+  }
+);
+
+
 module.exports = router;
