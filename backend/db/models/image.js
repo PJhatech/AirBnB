d08ac274fb4da62e44d491dbebd46490a7e33f7d
Paul Jha <paul.hutalla@gmail.com>
2023-01-17 16:58:57 -0800

    added polymporphic associations

--- backend/db/models/image.js
+++ backend/db/models/image.js
@@ -4,13 +4,21 @@
 const {Model} = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
 	class Image extends Model {
-		/**
-		 * Helper method for defining associations.
-		 * This method is not a part of Sequelize lifecycle.
-		 * The `models/index` file will call this method automatically.
-		 */
+		getImageable(options) {
+			if (!this.imageableType) return Promise.resolve(null);
+			const mixinMethodName = `get${this.imageableType}`;
+			return this[mixinMethodName](options);
+		}
+
 		static associate(models) {
-			// define association here
+			Image.belongsTo(models.Spot, {
+				foreignKey: "imageableId",
+				constraints: false,
+			});
+			Image.belongsTo(models.Review, {
+				foreignKey: "imageableId",
+				constraints: false,
+			});
 		}
 	}
 	Image.init(
