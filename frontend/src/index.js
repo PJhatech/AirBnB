b46b7eb9cc8c62f368381df1013932639540e0fb
Paul Jha <paul.hutalla@gmail.com>
2023-02-22 17:01:21 -0800

    frontend setup complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,20 @@
 import React from "react";
-
 import "./index.css";
-
 import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App";
-
 import configureStore from "./store";
+import {restoreCSRF, csrfFetch} from "./store/csrf";
 
 const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+	restoreCSRF();
+
+	window.csrfFetch = csrfFetch;
+	window.store = store;
+}
 
 if (process.env.NODE_ENV !== "production") {
 	window.store = store;
