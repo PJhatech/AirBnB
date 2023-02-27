98766416b11349b5223dd21513ac500c3dbce271
Paul Jha <paul.hutalla@gmail.com>
2023-02-27 15:46:46 -0800

    adjustments

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -29,12 +29,9 @@ function ProfileButton({user}) {
 		return () => document.removeEventListener("click", closeMenu);
 	}, [showMenu]);
 
-	const closeMenu = () => setShowMenu(false);
-
 	const logout = (e) => {
 		e.preventDefault();
 		dispatch(sessionActions.logout());
-		closeMenu();
 	};
 
 	const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
@@ -61,14 +58,12 @@ function ProfileButton({user}) {
 						<li>
 							<OpenModalButton
 								buttonText="Log In"
-								onButtonClick={closeMenu}
 								modalComponent={<LoginFormModal />}
 							/>
 						</li>
 						<li>
 							<OpenModalButton
 								buttonText="Sign Up"
-								onButtonClick={closeMenu}
 								modalComponent={<SignupFormModal />}
 							/>
 						</li>
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -2,10 +2,35 @@ import React from "react";
 import {NavLink} from "react-router-dom";
 import {useSelector} from "react-redux";
 import ProfileButton from "./ProfileButton";
+import OpenModalButton from "../OpenModalButton";
+import LoginFormModal from "../LoginFormModal";
+import SignupFormModal from "../SignupFormModal";
 import "./Navigation.css";
 
 function Navigation({isLoaded}) {
 	const sessionUser = useSelector((state) => state.session.user);
+
+	let sessionLinks;
+	if (sessionUser) {
+		sessionLinks = (
+			<li>
+				<ProfileButton user={sessionUser} />
+			</li>
+		);
+	} else {
+		sessionLinks = (
+			<li>
+				<OpenModalButton
+					buttonText="Log In"
+					modalComponent={<LoginFormModal />}
+				/>
+				<OpenModalButton
+					buttonText="Sign Up"
+					modalComponent={<SignupFormModal />}
+				/>
+			</li>
+		);
+	}
 
 	return (
 		<ul>
@@ -14,11 +39,7 @@ function Navigation({isLoaded}) {
 					Home
 				</NavLink>
 			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
+			{isLoaded && sessionLinks}
 		</ul>
 	);
 }
